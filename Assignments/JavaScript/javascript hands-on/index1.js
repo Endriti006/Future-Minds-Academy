5aab5fef70d147c30a686eac5cce2c3fb0d8aacc
Endriti Kryeziu <endriti006@gmail.com>
2023-12-24 20:18:18 +0100

    Update index1.js

--- Assignments/JavaScript/javascript hands-on/index1.js
+++ Assignments/JavaScript/javascript hands-on/index1.js
@@ -2,13 +2,9 @@
 let intervalId; // to store the interval ID for clearing later
 
 function startNotification(text) {
-    // Clear any existing notification
-    endNotification();
-
-    // Set interval to change notification every 1 second
     intervalId = setInterval(() => {
         document.title = (document.title === 'Javascript hands-on') ? text : 'Javascript hands-on';
-        changeFavicon((document.title === 'Javascript hands-on') ? 'icons8-rcoket-16.png' : 'bell-icon.png');
+        changeFavicon((document.title === 'Javascript hands-on') ? 'icons8-bell-16.png' : 'icons8-rocket-16.png');
     }, 1000);
 }
 
