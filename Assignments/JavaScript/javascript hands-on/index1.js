4013bf4c065f5997295bf1282e2cab13f8176ab2
Endriti Kryeziu <endriti006@gmail.com>
2023-12-24 20:34:04 +0100

    .

--- Assignments/JavaScript/javascript hands-on/index1.js
+++ Assignments/JavaScript/javascript hands-on/index1.js
@@ -1,10 +1,10 @@
 
-let intervalId; // to store the interval ID for clearing later
+
+let intervalId;
 
 function startNotification(text) {
     intervalId = setInterval(() => {
         document.title = (document.title === 'Javascript hands-on') ? text : 'Javascript hands-on';
-        changeFavicon((document.title === 'Javascript hands-on') ? 'icons8-bell-16.png' : 'icons8-rocket-16.png');
     }, 1000);
 }
 
