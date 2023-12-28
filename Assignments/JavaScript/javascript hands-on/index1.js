1e1719588ac93303d77fba9d1e89db1dd375c861
Endriti Kryeziu <endriti006@gmail.com>
2023-12-28 23:56:43 +0100

    Update index1.js

--- Assignments/JavaScript/javascript hands-on/index1.js
+++ Assignments/JavaScript/javascript hands-on/index1.js
@@ -1,17 +1,49 @@
-let intervalId;
-let isBellIcon = true;
-const faviconElement = document.getElementById('favicon');
 
-function startNotification(text) {
-    intervalId = setInterval(() => {
-        isBellIcon = !isBellIcon; 
-        faviconElement.href = isBellIcon ? 'icons8-bell-16.png' : 'icons8-rocket-16.png';
-        document.title = (document.title === 'Javascript hands-on') ? text : 'Javascript hands-on';
+let defualTitle = document.title;
+let faviconElement = document.querySelector("link[rel~='icon']");
+let defaultFavicon = faviconElement.href;
+
+let bellIconLink = getBellIconLink();
+
+let isInDefaultState = true;
+let notifactionInterval;
+
+function startNotification(message) {
+    if (notifactionInterval)
+        endNotification();
+
+    notifactionInterval = setInterval(() => {
+        if (isInDefaultState)
+            setToNotifaction(message);
+        else
+            resetToDefualt();
+
+        isInDefaultState = !isInDefaultState;
     }, 1000);
 }
 
 function endNotification() {
-    clearInterval(intervalId);
-    document.title = 'Javascript hands-on';
-    faviconElement.href = 'icons8-bell-16.png';
+    clearInterval(notifactionInterval);
+    resetToDefualt();
+}
+
+function resetToDefualt() {
+    document.title = defualTitle;
+    faviconElement.href = defaultFavicon;
+}
+
+function setToNotifaction(message) {
+    document.title = message;
+    faviconElement.href = bellIconLink;
+}
+
+function getBellIconLink(){
+    const canvas = document.createElement('canvas');
+    canvas.widh = 32;
+    canvas.height =32;
+    const context = canvas.getContext('2D');
+    context.font = '32px sans-serif';
+    context.fillText('a',0,30);
+
+    return canvas.toDataURL('image/png');
 }
