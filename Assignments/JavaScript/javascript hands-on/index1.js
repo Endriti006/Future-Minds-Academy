8b772447778428f00dd6faea70ba62082ff86db2
Endriti Kryeziu <endriti006@gmail.com>
2023-12-28 18:24:28 +0100

    Update index1.js

--- Assignments/JavaScript/javascript hands-on/index1.js
+++ Assignments/JavaScript/javascript hands-on/index1.js
@@ -1,9 +1,11 @@
-
-
 let intervalId;
+let isBellIcon = true;
+const faviconElement = document.getElementById('favicon');
 
 function startNotification(text) {
     intervalId = setInterval(() => {
+        isBellIcon = !isBellIcon; 
+        faviconElement.href = isBellIcon ? 'icons8-bell-16.png' : 'icons8-rocket-16.png';
         document.title = (document.title === 'Javascript hands-on') ? text : 'Javascript hands-on';
     }, 1000);
 }
@@ -11,4 +13,5 @@ function startNotification(text) {
 function endNotification() {
     clearInterval(intervalId);
     document.title = 'Javascript hands-on';
+    faviconElement.href = 'icons8-bell-16.png';
 }
