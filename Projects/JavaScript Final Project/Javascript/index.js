d4e3ec4f7761afc4f4a1dec5c1423b9f7368da29
Endriti Kryeziu <endriti006@gmail.com>
2024-02-01 19:50:13 +0100

    .

---
+++ Projects/JavaScript Final Project/Javascript/index.js
@@ -0,0 +1,6 @@
+import * as Notifaction from './notifaction.js';
+import * as Listings from './listings.js';
+import * as settings from './settings.js';
+import * as Sidebar from './sidebar.js';
+
+console.log("Hello World");
