456461f781efbaece7fad592886a3c56001b0e57
Endriti Kryeziu <endriti006@gmail.com>
2024-02-01 19:55:39 +0100

    Update notifaction.js

--- Projects/JavaScript Final Project/Javascript/notifaction.js
+++ Projects/JavaScript Final Project/Javascript/notifaction.js
@@ -1,1 +1,11 @@
 console.log("Hello World1");
+
+let defaultNotifaction=[
+    "New message",
+    "Last week's report",
+    "Abrakadabra",
+    "Hello, World Notifaction",
+    "Jane Doe wants to connect"
+];
+
+localStorage.setItem("notication",JSON.stringify(defaultNotifaction));
