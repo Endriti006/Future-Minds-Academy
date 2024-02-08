af87589c481aae0a8b7bc73b83b706e5cf66009c
Endriti Kryeziu <endriti006@gmail.com>
2024-02-08 18:19:56 +0100

    Update notifaction.js

--- Projects/JavaScript Final Project/Javascript/notifaction.js
+++ Projects/JavaScript Final Project/Javascript/notifaction.js
@@ -5,7 +5,51 @@ let defaultNotifaction=[
     "Last week's report",
     "Abrakadabra",
     "Hello, World Notifaction",
+    "Jane Doe wants to connect",
     "Jane Doe wants to connect"
 ];
 
-localStorage.setItem("notication",JSON.stringify(defaultNotifaction));
+localStorage.setItem("notifcation",JSON.stringify(defaultNotifaction));
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Retrieve data from local storage
+    const notificationData = localStorage.getItem("notifcation");
+
+    // Check if data exists in local storage
+    if (notificationData) {
+        const notifications = JSON.parse(notificationData);
+
+        // Update notification count
+        const notificationCount = notifications.length;
+        document.getElementById('notification-count').innerText = notificationCount;
+
+        // Get the notification box element
+        const notificationBox = document.querySelector('.notification-box');
+
+        // Iterate through notifications and add them to the notification box
+        notifications.forEach(notification => {
+            const li = document.createElement('li');
+            li.textContent = notification;
+            notificationBox.appendChild(li);
+        });
+    }
+});
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var notificationContainer = document.getElementById('notification');
+        var notificationBox = notificationContainer.querySelector('.notification-box');
+
+       
+        notificationContainer.addEventListener('click', function () {
+            notificationBox.style.visibility = (notificationBox.style.visibility === 'visible') ? 'hidden' : 'visible';
+        });
+
+        document.addEventListener('click', function (event) {
+            if (!notificationContainer.contains(event.target)) {
+                notificationBox.style.visibility = 'hidden';
+            }
+        });
+    });
+
