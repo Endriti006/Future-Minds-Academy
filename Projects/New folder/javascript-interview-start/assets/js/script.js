f22c88887f59d6f1916fb662b6f90892c982aafb
Endriti Kryeziu <endriti006@gmail.com>
2024-02-21 16:37:36 +0100

    .

---
+++ Projects/New folder/javascript-interview-start/assets/js/script.js
@@ -0,0 +1,10 @@
+let movies = Document.queryselector(".p-0 movieGernes");
+
+
+let movielist ='';
+
+allMovies.array.forEach(element => {
+    movielist += '<option value="${item.gerne}">${item.gerne}</option>';
+});
+
+movies.gernes.innerHTML = allMmovielistmovies;
