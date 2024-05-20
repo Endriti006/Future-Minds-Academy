5b765289714d01d2064cfe27420ec5a671069ac9
Endriti Kryeziu <endriti006@gmail.com>
2024-05-20 16:18:14 +0200

    Create Footer.jsx

---
+++ Assignments/React/first assignments/sassio/src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer-info-protofolio">
+      <div className="row">
+        <span className="left-side">
+          <p>Privacy & Cookie Policy | Terms of Service</p>
+        </span>
+        <span className="middle-side">
+          <p>© Copyright 2021. Sassio</p>
+        </span>
+        <span className="right-side">
+          <img src="assets/images/fb.png" alt="Facebook" />
+          <img src="assets/images/tw.png" alt="Twitter" />
+          <img src="assets/images/insta.png" alt="Instagram" />
+          <img src="assets/images/ln.png" alt="LinkedIn" />
+        </span>
+      </div>
+    </footer>
+  );
+}
