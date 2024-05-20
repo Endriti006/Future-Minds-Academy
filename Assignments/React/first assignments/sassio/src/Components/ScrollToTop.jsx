45de621c1df5fd3873c2d6a7ab3349565cb4832f
Endriti Kryeziu <endriti006@gmail.com>
2024-05-20 16:18:21 +0200

    Create ScrollToTop.jsx

---
+++ Assignments/React/first assignments/sassio/src/Components/ScrollToTop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ScrollToTop() {
+  return (
+    <div className="scroll-to-top">
+      <a href="index.html#top">
+        <span className="material-symbols-outlined arrow-upward">arrow_upward</span>
+      </a>
+    </div>
+  );
+}
