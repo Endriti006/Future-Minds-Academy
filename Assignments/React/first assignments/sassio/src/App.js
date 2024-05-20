0378da67c8feb10f19f517597f6a0c669f7d3a6c
Endriti Kryeziu <endriti006@gmail.com>
2024-05-20 16:17:59 +0200

    Update App.js

--- Assignments/React/first assignments/sassio/src/App.js
+++ Assignments/React/first assignments/sassio/src/App.js
@@ -8,6 +8,16 @@ import Hero from './Components/Hero';
 import Trusted from './Components/Trusted';
 import NewComponentName from './Components/NewComponentName';
 import Row from './Components/Row'
+import PowerfulFeatures from './Components/PowerfulFeatures';
+import CustomerSection from './Components/CustomerSection';
+import ArrowsSection from './Components/ArrowsSection';
+import MetricsSection from './Components/MetricsSection';
+import AppScreenshotSection from './Components/AppScreenshotSection';
+import PricingSection from './Components/PricingSection';
+import FAQSection from './Components/FaqSection';
+import DownloadAppSection from './Components/DownloadAppSection';
+import Footer from './Components/Footer';
+import ScrollToTop from './Components/ScrollToTop';
 
 function App() {
   return (
@@ -49,6 +59,63 @@ function App() {
        section3Title="Advanced Reporting"
        section3Text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut autem eum laudantium quas recusandae."
 />
+<PowerfulFeatures
+buttonText="OUR FEATURES"
+title="Powerful Features"
+feature1Title="Theme Made Easy"
+feature1Text="Our astonishing style structure makes your customization process a breeze"
+feature2Title="Powerful Design"
+feature2Text="With ready-to-use content you'll deploy your implementation in no time"
+feature3Title="Creative Content"
+feature3Text="Repetitive styles all across the web? Nevermind that! Shine with unique styles"
+/>
+<CustomerSection
+  buttonText="EXPLORE NEW PLACES"
+  header="What our customer says"
+  review1Title="Design Quality"
+  review1Text="The template is really nice and offers quite a large set of options. It’s beautiful and the coding is done quickly and seamlessly. Thank you!"
+  reviewer1Name="Devon Lane"
+  reviewer1Position="Team Leader, Paypal"
+  review2Title="Design Quality"
+  review2Text="The template is really nice and offers quite a large set of options. It’s beautiful and the coding is done quickly and seamlessly. Thank you!"
+  reviewer2Name="Theresa Webb"
+  reviewer2Position="Software Tester, Envato"
+/>
+<ArrowsSection
+  arrowWestClass="west"
+  arrowEastClass="east"
+/>
+<MetricsSection
+        components="773"
+        downloads="1254"
+        followers="14941"
+        newUser="1254"
+      />
+      <AppScreenshotSection
+        buttonText="EXPLORE NEW PLACES"
+        title="App ScreenShot"
+      />
+
+<PricingSection
+        plansButtonText="PLANS"
+        pricingTitle="Simple Pricing"
+        basicPlanText="Free"
+        professionalPlanText="$599.95"
+        businessPlanText="$999.95"
+        getStartedText="GET STARTED"
+      />
+      <FAQSection
+        buttonText="GET HELP (HELP CENTER)"
+        heading="Frequently asked questions."
+      />
+
+<DownloadAppSection
+        buttonText="START TODAY"
+        heading="Download the App"
+        paragraph="New features. New appearance. No risk and credit card required."
+      />
+        <Footer /> 
+        <ScrollToTop /> 
 
     </div>
   );
