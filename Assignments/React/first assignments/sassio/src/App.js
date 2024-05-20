929995055e0746035ac89717cc3e519b73131add
Endriti Kryeziu <endriti006@gmail.com>
2024-05-20 02:07:39 +0200

    Update App.js

--- Assignments/React/first assignments/sassio/src/App.js
+++ Assignments/React/first assignments/sassio/src/App.js
@@ -5,6 +5,9 @@ import './style-responsive.css';
 import './style.css';
 import Nav from './Components/Nav';
 import Hero from './Components/Hero';
+import Trusted from './Components/Trusted';
+import NewComponentName from './Components/NewComponentName';
+import Row from './Components/Row'
 
 function App() {
   return (
@@ -18,6 +21,35 @@ function App() {
         buttonDownload="Get it on"
         appleText="Google Play"
       />
+      <Trusted paragraph="Trusted by the world's best" />
+      <NewComponentName
+        button1Text="SAFETY FIRST"
+        heading1="You're in a secure company"
+        section1Title="Real time activity"
+        section1Paragraph="Get instant insight on what's happening in your business. It uses a dictionary of the over 200 Latin words."
+        section2Title="Instant deploy"
+        section2Paragraph="Deploy your project in a fraction of a second, no time! It uses a dictionary of the over 200 Latin words."
+        section3Title="Easy integrations"
+        section3Paragraph="Integrate our solution with other tech leaders in the industry. It uses a dictionary of the over 200 Latin words."
+        section4Title="Perfect UI/UX"
+        section4Paragraph="Get instant insight on what's happening in your business. It uses a dictionary of the over 200 Latin words."
+        benefitButtonText="SAFETY FIRST"
+        benefitHeading="Tons of benefit"
+        benefitItems1={["Development", "Web Design", "Do Magic", "Save Time"]}
+        benefitItems2={["App builder", "Dashboard", "Feedback"]}
+        freeTrialButtonText="FREE TRIAL"
+      />
+      <Row
+       title="Path to Success"
+       buttonText="WE ARE YOUR ONLY"
+       section1Title="Mail Management"
+       section1Text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut autem eum laudantium quas recusandae."
+       section2Title="Customers Tracking"
+       section2Text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut autem eum laudantium quas recusandae."
+       section3Title="Advanced Reporting"
+       section3Text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut autem eum laudantium quas recusandae."
+/>
+
     </div>
   );
 }
