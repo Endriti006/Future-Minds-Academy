72cd88ab62bda8f858af46d996098d7fe3eb6038
Endriti Kryeziu <endriti006@gmail.com>
2024-05-25 01:21:12 +0200

    Create LgAbout.jsx

---
+++ Assignments/React/first assignments/sassio/src/Components/AboutFile/LgAbout.jsx
@@ -0,0 +1,22 @@
+export default function LgAbout(){
+    return(
+        <div className="wrapper-lg">
+        <footer className="footer-info-protofolio ">
+           <div className="row">
+              <span className="left-side">
+                 <p>Privacy & Cookie Policy | Terms of Service</p>
+              </span>
+              <span className="middle-side">
+                 <p>© Copyright 2021. Sassio</p>
+              </span>
+              <span className="right-side">
+                 <img src="assets/images/fb.png" alt=""/>
+                 <img src="assets/images/tw.png" alt=""/>
+                 <img src="assets/images/insta.png" alt=""/>
+                 <img src="assets/images/ln.png" alt=""/>
+              </span>
+           </div>
+        </footer>
+     </div>
+    );
+}
