f195e6e4fcd018d9204678eba95ca91f0f977e2c
Endriti Kryeziu <endriti006@gmail.com>
2024-05-25 01:21:18 +0200

    Create WrapperAbout.jsx

---
+++ Assignments/React/first assignments/sassio/src/Components/AboutFile/WrapperAbout.jsx
@@ -0,0 +1,16 @@
+export default function WrapperAbout(){
+    return(
+    <div className="wrapper-xl text-center">
+    <div className="about-section">
+       <img src="assets/images/bg.png" alt="Background Image" className="bg-img about" />
+       <h1 className="dark">About us</h1>
+       <br />
+       <p>
+          With we want to optimize the customization process so your team
+          <br />
+          save time when building websites.
+       </p>
+    </div>
+ </div>
+    );
+}
