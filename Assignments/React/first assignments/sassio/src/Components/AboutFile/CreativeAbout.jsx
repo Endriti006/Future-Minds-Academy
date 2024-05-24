e55a63209de8c770a199dcdeac62617baf6a5820
Endriti Kryeziu <endriti006@gmail.com>
2024-05-25 01:21:02 +0200

    Create CreativeAbout.jsx

---
+++ Assignments/React/first assignments/sassio/src/Components/AboutFile/CreativeAbout.jsx
@@ -0,0 +1,55 @@
+export default function CreativeAbout(){
+    return(
+        <div className="wrapper-lg creative">
+        <button className="speciale-1 team-tag">TEAM</button>
+        <h1 className="creative-header">We develop & create digital future.</h1><br/>
+        <div className="row middle">
+           <div className="creative-section">
+              <div className=" persons-info">
+                 <div className="persons-img">
+                    <img src="assets/images/person.png"/>
+                 </div>
+                 <div className="person-details">
+                    <p className="dark">Albert Flores</p>
+                    <p>President of Sales</p>
+                 </div>
+              </div>
+              <div className=" persons-info">
+                 <div className="persons-img">
+                    <img src="assets/images/person.png"/>
+                 </div>
+                 <div className="person-details">
+                    <p className="dark">Albert Flores</p>
+                    <p>President of Sales</p>
+                 </div>
+              </div>
+              <div className=" persons-info">
+                 <div className="persons-img">
+                    <img src="assets/images/person.png"/>
+                 </div>
+                 <div className="person-details">
+                    <p className="dark">Albert Flores</p>
+                    <p>President of Sales</p>
+                 </div>
+              </div>
+              <div className=" persons-info">
+                 <div className="persons-img">
+                    <img src="assets/images/person.png"/>
+                 </div>
+                 <div className="person-details">
+                    <p className="dark">Albert Flores</p>
+                    <p>President of Sales</p>
+                 </div>
+              </div>
+           </div>
+        </div>
+        <div className="bullet-container">
+           <div className="bullet"></div>
+           <div className="bullet"></div>
+           <div className="bullet"></div>
+           <div className="bullet"></div>
+           <div className="bullet"></div>
+        </div>
+     </div>
+    );
+}
