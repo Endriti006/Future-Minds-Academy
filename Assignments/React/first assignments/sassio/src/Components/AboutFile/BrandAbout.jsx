83dbd593101eb5274f5692986ca89f5d6d72c3c8
Endriti Kryeziu <endriti006@gmail.com>
2024-05-25 01:20:56 +0200

    Create BrandAbout.jsx

---
+++ Assignments/React/first assignments/sassio/src/Components/AboutFile/BrandAbout.jsx
@@ -0,0 +1,28 @@
+export default function BrandAbout(){
+    return(
+        <div className="wrapper-lg brands-section about">
+        <button className="speciale-1 team-tag">PARTNERS</button>
+        <h1 className="creative-header">We work with the best brands</h1><br/>
+        <div className="row brands">
+           <div className="col-end m-5">
+              <img src="assets/images/amazon.png"/>
+           </div>
+           <div className="col-end m-5">
+              <img src="assets/images/amd.png"/>
+           </div>
+           <div className="col-end m-5">
+              <img src="assets/images/cisco.png"/>
+           </div>
+           <div className="col-end m-5">
+              <img src="assets/images/dropcam.png"/>
+           </div>
+           <div className="col-end m-5">
+              <img src="assets/images/logitech.png"/>
+           </div>
+           <div className="col-end m-5">
+              <img src="assets/images/spotify.png"/>
+           </div>
+        </div>
+     </div>
+    );
+}
