1e3bffe28eb52d768e48eb1fc4c91ceeca691e60
Endriti Kryeziu <endriti006@gmail.com>
2024-05-25 16:14:16 +0200

    Create ServicesHero.jsx

---
+++ Assignments/React/first assignments/sassio/src/Components/Services/ServicesHero.jsx
@@ -0,0 +1,16 @@
+export default function ServicesHero(){
+    return(
+        <div className="wrapper-xl text-center hero-services">
+        <div className="services-section">
+           <img src="assets/images/bg.png" alt="Background Image" className="bg-img services" />
+           <h1 className="dark">Our Services</h1>
+           <br />
+           <p className="services-text">
+              We are a professional digital agency providing premium themes, UI/UX <br/> design and web apps that
+              you
+              will simply love.
+           </p>
+        </div>
+     </div>
+    );
+}
