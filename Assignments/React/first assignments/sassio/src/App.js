9a10d87dda8748ed85a7f088d0531e487f73d871
Endriti Kryeziu <endriti006@gmail.com>
2024-05-26 02:26:47 +0200

    Update App.js

--- Assignments/React/first assignments/sassio/src/App.js
+++ Assignments/React/first assignments/sassio/src/App.js
@@ -5,23 +5,26 @@ import './style-responsive.css';
 import './style.css';
 
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-
-
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Services from './Pages/Services';
-
+import Portfolio from './Pages/Portofolio';
+import Contact from './Pages/Contact';
+import Blog from './Pages/Blog';
 
 const router = createBrowserRouter([
   {path: '/', element: <Home />},
   {path: '/about', element: <About />},
-  {path: '/services', element: <Services />}
+  {path: '/services', element: <Services />},
+  {path: '/portfolio', element: <Portfolio />},
+  {path: '/contact', element: <Contact/>},
+  {path: '/blog' , element: <Blog/>}
 ]);
 
 function App() {
   return (
     <div className="App">
-    <RouterProvider router={router}/>
+      <RouterProvider router={router}/>
     </div>
   );
 }
