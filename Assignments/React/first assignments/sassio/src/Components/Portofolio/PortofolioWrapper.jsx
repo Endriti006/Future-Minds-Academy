df305f1c2a24f90b3cf94c2d28c924f3b7d68d44
Endriti Kryeziu <endriti006@gmail.com>
2024-05-26 02:27:15 +0200

    Create PortofolioWrapper.jsx

---
+++ Assignments/React/first assignments/sassio/src/Components/Portofolio/PortofolioWrapper.jsx
@@ -0,0 +1,86 @@
+export default function PortfolioWrapper(){
+    return(
+        <div class="wrapper-md">
+        <div class="row portofolio-intro">
+            <div class="col-1 text-center portofolio-info">
+                <h1 class="dark portofolio">Portfolio Showcase</h1>
+                <p class="portofolio-paragraf">With we want to optimize the customization process so your
+                    team<br/>can
+                    save time when building websites.
+                </p>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col navigation-protofolio">
+                <a href="#">All Project</a>
+                <a href="#">E-commerce</a>
+                <a href="#">Digital Design</a>
+                <a href="#">Branding</a>
+                <a href="#">Web Design</a>
+            </div>
+        </div>
+        <div class="services-protofolio">
+            <div class="row protofolio">
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>Billboard</h3>
+                </div>
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>Creative Science</h3>
+                </div>
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>Air Plane</h3>
+                </div>
+            </div>
+            <div class="row protofolio">
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>Lewis Baker</h3>
+                </div>
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>AG. Avent</h3>
+                </div>
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>Virtual Payment</h3>
+                </div>
+            </div>
+            <div class="row protofolio">
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>Fortune Art</h3>
+                </div>
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>Good Food</h3>
+                </div>
+                <div class="col services-protofolio-info">
+                    <img src="assets/images/portofolio-img.png" alt="Project 1"/>
+                    <p>#marketing #design #development</p>
+                    <h3>Enterprice Silver</h3>
+                </div>
+            </div>
+        </div>
+        <div class="launch-section-protofolio text-center">
+            <h1 class="display-1"> We help startups launch and succeed very fast</h1>
+            <p class="launch-text-protofolio">We appreciate your trust greatly! Our clients choose us and our
+                products because
+                they<br/> know we’re the
+                best.
+            </p>
+            <button class="pro-btn">GET STARTED</button><br/>
+        </div>
+    </div>
+    )
+}
