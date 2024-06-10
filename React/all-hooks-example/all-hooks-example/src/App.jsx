f8d4fe98eff52607772c5b9290c32dc31081c0df
Endriti Kryeziu <endriti006@gmail.com>
2024-06-11 00:08:38 +0200

    Update App.jsx

--- React/all-hooks-example/all-hooks-example/src/App.jsx
+++ React/all-hooks-example/all-hooks-example/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import Counter from './views/Counter'
 
 import ToggleVisibility from './views/ToggleVisibility';
+import InputField from './views/InpurtField';
 
 
 const router = createBrowserRouter([
   {path: '/', element: <Counter />},
   {path: '/toggle-visibility', element: <ToggleVisibility />},
-  {path: '/input-field', element: <Counter />}
+  {path: '/input-field', element: <InputField />}
 
 ]);
 
