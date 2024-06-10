421e8ebaaa5bdb9c89168db4f980cdb6f24f28e5
Endriti Kryeziu <endriti006@gmail.com>
2024-06-11 00:08:44 +0200

    Create InpurtField.jsx

---
+++ React/all-hooks-example/all-hooks-example/src/views/InpurtField.jsx
@@ -0,0 +1,10 @@
+import InputFieldH from "../components/Hooks/InputFieldH";
+import Nav from "../components/Nav/Nav";
+export default function InputField(){
+    return(
+        <>
+        <Nav />
+        <InputFieldH/>
+        </>
+    );
+}
