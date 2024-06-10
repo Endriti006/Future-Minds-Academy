4a61a15946c29faaa9a29a7b2ce9c3a9ada69aca
Endriti Kryeziu <endriti006@gmail.com>
2024-06-10 23:45:16 +0200

    Create App.jsx

---
+++ React/all-hooks-example/all-hooks-example/src/App.jsx
@@ -0,0 +1,26 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import './App.css'
+import Counter from './views/Counter'
+
+import ToggleVisibility from './views/ToggleVisibility';
+
+
+const router = createBrowserRouter([
+  {path: '/', element: <Counter />},
+  {path: '/toggle-visibility', element: <ToggleVisibility />},
+  {path: '/input-field', element: <Counter />}
+
+]);
+
+function App() {
+
+
+  return (
+    <>
+     <h1>React Hooks</h1>
+     <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
