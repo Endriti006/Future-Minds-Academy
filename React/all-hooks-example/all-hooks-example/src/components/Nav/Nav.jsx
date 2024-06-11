afc6e9fe2440adb9139383c4d477a3d01d61a4fa
Endriti Kryeziu <endriti006@gmail.com>
2024-06-11 16:38:22 +0200

    Update Nav.jsx

--- React/all-hooks-example/all-hooks-example/src/components/Nav/Nav.jsx
+++ React/all-hooks-example/all-hooks-example/src/components/Nav/Nav.jsx
@@ -7,6 +7,8 @@ export default function Nav() {
                 <li><a href="/">1. Counter</a></li>
                 <li><a href="toggle-visibility">2. toggle-visibility</a></li>
                 <li><a href="/input-field">3. Input Field</a></li>
+                <li><a href="/handling">4. Handling</a></li>
+                <li><a href="/array">5. Array of Items</a></li>
             </ul>
         </nav>
         </>
