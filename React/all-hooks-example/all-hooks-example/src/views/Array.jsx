47cea052a55cc5fdc021db679d36f94f7720c83d
Endriti Kryeziu <endriti006@gmail.com>
2024-06-11 16:38:33 +0200

    Create Array.jsx

---
+++ React/all-hooks-example/all-hooks-example/src/views/Array.jsx
@@ -0,0 +1,12 @@
+import ArrayH from "../components/Hooks/ArrayH";
+import Nav from "../components/Nav/Nav";
+
+export default function Array(){
+    return(
+        <>
+            <Nav/>
+            <ArrayH/>
+        </>
+
+    );
+}
