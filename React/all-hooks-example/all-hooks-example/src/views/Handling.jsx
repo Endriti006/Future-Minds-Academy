485de2331c73d1cf4e91d471a6d66008adfa6ef2
Endriti Kryeziu <endriti006@gmail.com>
2024-06-11 16:38:35 +0200

    Create Handling.jsx

---
+++ React/all-hooks-example/all-hooks-example/src/views/Handling.jsx
@@ -0,0 +1,11 @@
+import HandlingH from "../components/Hooks/HandlingH";
+import Nav from "../components/Nav/Nav";
+
+export default function Handling(){
+    return(
+        <>
+        <Nav/>
+        <HandlingH/>
+        </>
+    );
+}
