9242a04869086baf3c70f192f4bfaaf277961ee6
Endriti Kryeziu <endriti006@gmail.com>
2024-09-26 15:40:09 +0200

    Create library.sql

---
+++ Assignments/Node/Build a Book Management System/library.sql
@@ -0,0 +1,12 @@
+create database library
+
+use library
+
+create table books(
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(50),
+author VARCHAR(50),
+genre VARCHAR(50),
+rating VARCHAR(50),
+year INT
+);
