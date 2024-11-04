6c6cfb5745e5301d36253dc4390ab72c1099c094
Endriti Kryeziu <93491718+Endriti006@users.noreply.github.com>
2024-11-04 11:02:51 +0100

    .

---
+++ Node/Session9/index.js
@@ -0,0 +1,13 @@
+const express= require("express");
+const authController = require('./controllers/authController')
+
+const app = express();
+app.use(express.json());
+
+
+app.use("/auth", authController)
+
+
+
+app.listen(8585);
+
